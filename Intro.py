02bd2c7db42a158e1d3f28797eb1f1f0376e73e7
DevoloperGurveer <gurveercodingal14@gmail.com>
2025-01-29 17:31:01 +0530

    Add files via upload

---
+++ Intro.py
@@ -0,0 +1,14 @@
+#comment syntax - structure - print("")
+
+print("My name is Gurveer")
+
+print("I am Snake(Python) developer")
+
+print("But they don't bite me because I have never seen one")
+
+print("I also don't want to see a snake becaue they are horrific.\n In India we find Cobras but no Python")
+
+
+
+
+
