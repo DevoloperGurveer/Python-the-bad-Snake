85eab3397f7e4d98434cd04c9c54fce37e8d4e5e
DevoloperGurveer <gurveercodingal14@gmail.com>
2025-02-03 17:37:42 +0530

    Add files via upload

---
+++ Today.py
@@ -0,0 +1,38 @@
+a=5896747
+print("type of a:",type(a))
+x=float(a) 
+print("type of x:",type(x))
+
+b=46327496427968.4638375217845815463812541
+print("type of b:",type(b))
+y=int(b)
+print("type of y:",type(y))
+
+c="Pythons are ugly"
+print("type of c:",type(c))
+#z=int(c)
+#print("type of z:",type(z))
+
+uppercase=c.upper()
+print(uppercase)
+print(c[::-1])
+
+
+
+
+d=True
+print("type of d:",type(d))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
