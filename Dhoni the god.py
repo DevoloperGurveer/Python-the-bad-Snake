abf252f06dcbe2b9f87a72a9eb21b7383cb0ea0a
DevoloperGurveer <gurveercodingal14@gmail.com>
2025-02-05 17:48:29 +0530

    Add files via upload

---
+++ Dhoni the god.py
@@ -0,0 +1,22 @@
+print("Enter marks obtained in 7 subjects:")
+math=int(input("maths:"))
+English=int(input("English:"))
+Hindi=int(input("Hindi:"))
+Sanskrit=int(input("Sanskrit:"))
+Science=int(input("Science:"))
+Sst=int(input("Social Studies:"))
+Comp=int(input("Computer Science:"))
+sum=math+English+Hindi+Sanskrit+Science+Sst+Comp
+print("sum of math,hindi,sanskrit,science,sst and computers")
+ 
+perc=(sum/700)*100
+
+print(end="Percentage mark=")
+
+print(perc)
+
+
+
+
+
+
