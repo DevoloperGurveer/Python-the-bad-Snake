ea4b49b6c71979c8a1b73ab69e55714fef3a4194
DevoloperGurveer <gurveercodingal14@gmail.com>
2025-02-10 17:40:19 +0530

    Add files via upload

---
+++ If.py
@@ -0,0 +1,31 @@
+a=0
+d=6767473146774624164616446884648636836838686328668
+f=788286464241111124644444444444444444444444444444444444444444444444444444444488888888888888888888888888
+
+if a and d or f: 
+    print("All the operators have their boolean value as true.")
+else:
+    print("At least one of the operators have their boolen value as false.")
+    
+
+
+
+
+
+
+
+if a and d and f: 
+    print("All the operators have their boolean value as true.")
+else:
+    print("At least one of the operators have their boolen value as false.")
+    
+
+
+
+
+
+
+
+
+
+
---
+++ Not.py
@@ -0,0 +1,17 @@
+a=int(input("enter a number:"))
+if a%2!=0:
+    print(a,"is not an even number.")
+else:
+    print("is an even number")
+
+
+
+
+
+
+
+
+
+
+
+
