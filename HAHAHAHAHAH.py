55f5bb24abed2f176ae7b8dbd2cca8fa93d35e32
DevoloperGurveer <gurveercodingal14@gmail.com>
2025-03-12 23:21:24 +0530

    Add files via upload

---
+++ HAHAHAHAHAH.py
@@ -0,0 +1,7 @@
+import math
+print("The floor and Ceiling value of 23.56 are:"+str(math.ceil(23.56))+','+str(math.floor(23.56)))
+x=10
+y=-15
+print("The value of x after copying the cell of y is:"+ str (math.copysign(x,y)))
+print("the absolute value of 56 and -96 are:"+str(math.fabs(-96))+','+str(math.fabs(56)))
+print ("The GCD of -96 and 56 are:"+str(math.gcd(24,56)))
