5a028eed7ebc077c1b47414feac0bc287d5cb474
DevoloperGurveer <gurveercodingal14@gmail.com>
2025-03-13 20:22:33 +0530

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,6 @@
+from datetime import date , time , datetime
+today=date.today()
+now=datetime.now()
+print("Today's date is",today)
+print("\n The Current and time is",now)
+print("\n Date Compents",today.year,today.month,today.day)
