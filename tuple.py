298800aaa3e52523ea3af4ba007311ef1a35f234
DevoloperGurveer <gurveercodingal14@gmail.com>
2025-03-17 17:42:46 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,14 @@
+tuplex=("tuple" ,True,False,5.3,2)
+print(tuplex)
+tupley= (4,9,2,3,1,5,7,6,8)
+print(tupley)
+tuples=tupley + (9)
+print(tuples)
+tuple
+tuple1=(30,40,50,20,60,70,90)
+print(tuple1.count(50))
+tupler=(1,2,3,4,5,6,7,8,9,0)
+_slice=tupler[4:9]
+print(_slice)
+_slice=tupler[:7]
+print(_slice)
