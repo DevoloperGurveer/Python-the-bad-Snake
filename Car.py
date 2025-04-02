a4a0d16c524d4ac33cf20ec5a8590f1cd6167996
DevoloperGurveer <gurveercodingal14@gmail.com>
2025-04-02 17:36:37 +0530

    Add files via upload

---
+++ Car.py
@@ -0,0 +1,9 @@
+class vehichle:
+    def __init__(self,Max_Speed,Milage):
+        self.max_speed=Max_Speed
+        self.milage=Milage
+modelS=vehichle(240,18)
+print("Model max speed",modelS.max_speed)
+print("Model milage",modelS.milage)
+
+        
